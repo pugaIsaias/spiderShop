546524f93b573be0c6d49cd06f76b0000e175428
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-01-26 18:22:21 -0600

    add Routing features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Layout from "./containers/Layout/Layout";
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <div>
+      <Layout>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" exact component={Dashboard} />
+        </Switch>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navigation/NavItems/NavItem/NavItem.js
+++ src/components/Navigation/NavItems/NavItem/NavItem.js
@@ -1,9 +1,12 @@
-import classes from "-/NavItem.module.css";
+import { NavLink } from "react-router-dom";
+import classes from "./NavItem.module.css";
 
 const navItem = (props) => {
   return (
     <li className={classes.NavItem}>
-      <a href={props.link}>{props.children}</a>
+      <NavLink to={props.link} exact activeClassName={classes.active}>
+        {props.children}
+      </NavLink>
     </li>
   );
 };

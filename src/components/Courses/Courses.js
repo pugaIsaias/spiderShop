0d887c2d896060a9546fc12746e38e3f04433df9
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-02-05 22:52:29 -0600

    using axios to fill courses

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -9,32 +9,24 @@ const Courses = () => {
   axios
     .get("https://jsonplaceholder.typicode.com/photos/")
     .then((response) => {
-      let products = response.data.filter((data) => data.id < 10);
+      let filterData = response.data.filter((data) => data.id < 10);
+      let photos = [];
+
+      filterData.forEach((element) => {
+        let fragment = {};
+        fragment.id = element.id;
+        fragment.name = element.title;
+        fragment.price = element.id + 10;
+        fragment.banner = element.thumbnailUrl;
+        fragment.link = element.id;
+        photos.push(fragment);
+      });
+
+      setCourseList(photos);
     })
     .catch((error) => {
       console.log("axios error:", error);
     });
-
-  /* const [courseList, setCourseList] = useState([
-    {
-      id: 0,
-      name: "taller 5 lenguajes del amor",
-      description: "taller sobre los 5 lenguajes del amor",
-      price: "Q25",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/rotaract-metropoli-gt.appspot.com/o/love.jpg?alt=media&token=e8b6e881-8460-4c82-a76f-eb819ad9bd1e",
-      link: "/4255",
-    },
-    {
-      id: 1,
-      name: "curso de bartender",
-      description: "aprende a hacer 3 tipos de tragos diferentes",
-      price: "Q35",
-      banner:
-        "https://firebasestorage.googleapis.com/v0/b/rotaract-metropoli-gt.appspot.com/o/coctails.png?alt=media&token=2c6f3536-5d7c-4bc8-95db-e29ddb6b25c7",
-      link: "/3576",
-    },
-  ]);*/
 
   return (
     <>

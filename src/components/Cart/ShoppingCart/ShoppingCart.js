bd3607ba035e142239ad0c3ca60e9db02c83535a
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-05-04 21:22:02 -0600

    better styling for cart

--- src/components/Cart/ShoppingCart/ShoppingCart.js
+++ src/components/Cart/ShoppingCart/ShoppingCart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LocalStatehooks from "./LocalStatehooks";
+import classes from "./ShoppingCart.module.css";
 
 const ShoppingCart = (props) => {
   //TODO: set a state for localStorage
@@ -9,16 +10,24 @@ const ShoppingCart = (props) => {
 
   let readable = JSON.parse(cart);
   return (
-    <>
-      <p>
-        Items on Cart : {readable.length}
-        {readable.map((course) => (
-          <div>
-            <img src={course.banner} alt="" /> {course.name}
-          </div>
-        ))}
-      </p>
-    </>
+    <div className={classes.cart}>
+      <p>Items on Cart : {readable.length}</p>
+      {readable.map((course) => (
+        <div className={classes.cartItem}>
+          <img className={classes.cartImg} src={course.banner} alt="" />{" "}
+          {course.name}
+          <p className={classes.cartItemPrice}> Q {course.price}</p>
+        </div>
+      ))}
+
+      <button className={classes.cartButton}>
+        Checkout Q
+        {readable.reduce(
+          (accumulator, currentValue) => accumulator + currentValue.price,
+          0
+        )}
+      </button>
+    </div>
   );
 };
 
